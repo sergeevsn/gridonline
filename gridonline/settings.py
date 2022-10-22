afb17cebdee8874e564a71ee87bb555493137678
Сергеев Сергей <sergeew.sergey@gmail.com>
2022-10-22 19:04:50 +0200

    Heroku Deploy

--- gridonline/settings.py
+++ gridonline/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = os.environ['SECRET_KEY'] # Instead of your actual secret key
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gridonline.herokuapp.com','127.0.0.1']
 
 
 # Application definition
